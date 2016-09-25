271a516bcb0b652d886bb56e0900d3f470d3fb17
xjavorkas <samo.javorka@gmail.com>
2016-09-25 21:43:18 +0200

    Samuel Javorka, zadanie1 - cut

--- Application/Application.cpp
+++ Application/Application.cpp
@@ -1,12 +1,12 @@
-﻿// Application.cpp : Defines the entry point for the console application.
-//
+﻿// ConsoleApplication1.cpp : Defines the entry point for the console application.
+// Samuel Javorka, orezanie (cut)
 
 #include "stdafx.h"
 #include <stdlib.h>
 #include "..\Library\Library.h"
 #include <iostream>
 
-void DisplayUsage( char * appPath)
+void DisplayUsage(char * appPath)
 {
 	char drive[_MAX_DRIVE];
 	char dir[_MAX_DIR];
@@ -16,28 +16,39 @@ void DisplayUsage( char * appPath)
 	_splitpath_s(appPath, drive, dir, fname, ext);
 	std::cout << fname << std::endl;
 	std::cout << "\t-h - help" << std::endl;
-	std::cout << "\t-t \"text na vypis\" - vypis textu definovaneho parametrom" << std::endl;
 }
+
+/*
+max - kolko znakov/segmentov chceme vypísať
+delim - ak != 0, tak potom delitel segmentov
+*/
+void cut(unsigned int max, int delim);
 
 int main(int argc, char **argv)
 {
 	int c;
-
+	char *ptr;
+	int delim = 0;
+	int max = 0;
 	if (argc == 1)
 	{
 		DisplayUsage(argv[0]);
 	}
 	else
 	{
-		while ((c = getopt(argc, argv, "ht:" )) != EOF)
+		while ((c = getopt(argc, argv, "hd:m:")) != EOF)
 		{
 			switch (c)
 			{
 			case 'h':
 				DisplayUsage(argv[0]);
+				return 0;
+			case 'd':
+				// prvý znak zo špecifikacie je oddelovať
+				delim = optarg[0];
 				break;
-			case 't':
-				std::cout << "Text: " << optarg << std::endl;
+			case 'm':
+				max = strtol(optarg, &ptr, 10);
 				break;
 			case '?':
 				std::wcout << "Chybny parameter '" << argv[optind - 1] << "'" << std::endl;
@@ -50,6 +61,35 @@ int main(int argc, char **argv)
 			}
 		}
 	}
-    return 0;
+
+
+	cut(max, delim);
+	std::cout << std::endl;
+	return 0;
 }
 
+void cut(unsigned int max, int delim) {
+	// ak nešpecifikujeme max, vypíše prvý segment/znak
+	if (!max)
+		max = 1;
+	unsigned int charCounter = 0;
+	unsigned int segmentCounter = 0;
+	// read vstup, znak po znaku
+	for (int ch = 0; (ch = fgetc(stdin)) != EOF; ++charCounter)
+	{
+		if (ch == delim)
+		{
+			++segmentCounter;
+			// continue; //odkomentovat ak chceme vypisat bez delicu
+		}
+
+		if ((!delim && charCounter < max) || delim && segmentCounter < max)
+		{
+			putchar(ch);
+		}
+
+		// možme breaknut, nečitame dalej
+		else
+			break;
+	}
+}
